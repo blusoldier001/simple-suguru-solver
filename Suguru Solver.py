7fe91ceff3b571f29b81afc8d9519a8279d6af0c
Jason L <35697422+blusoldier001@users.noreply.github.com>
2022-11-11 12:14:02 -0600

    Initial commit

---
+++ Suguru Solver.py
@@ -0,0 +1,11 @@
+import numpy as np
+#https://www.youtube.com/watch?v=BY5O1-R-c8o&t=25s&ab_channel=dkmgames
+
+sample_array = np.array([[0,0,1,0,0,0],[2,0,0,0,0,5],[0,0,0,0,0,0],[1,0,0,0,0,0],[0,0,0,0,0,0],[0,0,1,0,2,3]])
+group1 = np.array([[0,0],[0,1],[1,0],[1,1],[1,2]])
+group2 = np.array([[0,2],[0,3],[0,4],[1,3],[1,4]])
+group3 = np.array([[0,5],[1,5],[2,5],[3,5],[3,4]])
+group4 = np.array([[2,0],[2,1],[2,2],[3,0],[3,1]])
+group5 = np.array([[2,3],[2,4],[3,3]])
+group6 = np.array([[4,0],[5,0],[5,1]])
+group7 = np.array([[]])
